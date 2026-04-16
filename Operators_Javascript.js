cac1cd9f77ccee6d3df522907495f26f4581743d
Tanvee Dighe <tanvidighe1356@gmail.com>
2026-04-16 21:11:23 +0530

    Add JavaScript operators examples in Operators_Javascript.js

---
+++ Operators_Javascript.js
@@ -0,0 +1,56 @@
+// ! Operators
+
+// // ! Arithmatic operators - +,-,*,/,%,++,--
+// console.log(30+90);
+// console.log("hello"+"hiii");
+// console.log(30+'90');
+// console.log(60-45);
+// console.log(5*4);
+// console.log(40/2);
+// console.log(50%3);
+
+// let a = 10  //10
+// // a++ , ++a , --a , a--
+// // console.log(a++);
+// // console.log(++a);
+// // console.log(a--);
+// // console.log(--a);
+
+// console.log(a-- + ++a + a++ + --a);
+//          10   + 10 + 10  + 10
+
+// ! Assignment operators - =,+=,-=,*=,/=,%=
+// let x = 100
+
+// x += 10  // x=x+10
+// console.log(x);
+
+// x += "10"  // x=x+"10"
+// console.log(x);
+
+// x -= 10  // x=x-10
+// console.log(x);
+
+// x *= 10  // x=x*10
+// console.log(x);
+
+// x /= 10  // x=x/10
+// console.log(x);
+
+// x %= 10  // x=x%10
+// console.log(x);
+
+// ! comparison operators - >,<,>=,<=,!=,==,===
+console.log(4>12);
+console.log(3<56);
+console.log(2>=10);
+console.log(4<=4);
+console.log(2!=20);
+
+// == -- compares only values 
+console.log( 40 == 40 );
+console.log( 40 == '40' );
+
+// === -- compares both datatype and values
+console.log( 40 === 40 );
+console.log( 40 === '40' ); 
