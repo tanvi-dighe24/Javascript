16b2df34240e7778079bd2e4b7ef347038703b09
Tanvee Dighe <tanvidighe1356@gmail.com>
2026-04-17 22:08:10 +0530

    Add initial comments for loop types in Loops.js

---
+++ Loops.js
@@ -0,0 +1,25 @@
+//While loop , Do while loop, For loop
+
+
+
+
+// ! loops -- initialization condition updation
+
+// while : entry controlled loop
+let a = 1
+while(a<=10){
+    document.writeln("Good evening✅");
+    a++
+}
+
+// do while : exit conrolled loop
+let x = 10
+do{
+    document.writeln("Today was the great day.😍")
+    x--
+}while(x>0)
+
+// for
+for(let i=1;i<=100;i++){
+    document.writeln("Good Night💖")
+} 
