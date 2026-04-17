ac135d304d0a692ae63cce7d7c37e760d817b1d0
Tanvee Dighe <tanvidighe1356@gmail.com>
2026-04-17 22:02:57 +0530

    Add examples for logical and ternary operators

--- Operators_Javascript.js
+++ Operators_Javascript.js
@@ -54,3 +54,19 @@ console.log( 40 == '40' );
 // === -- compares both datatype and values
 console.log( 40 === 40 );
 console.log( 40 === '40' ); 
+
+
+// ! logical operators -- &&,||,!
+
+// // && - all the conditions are true -- true
+// console.log(5<30 && 300>=90 && 1<10);
+
+// // || - any one condition is true -- true  
+// console.log(4<9 || 30>=100 || 300<=200 || 20==20);
+
+// // ! -- true - false
+// console.log(!(20>10));
+
+// ! ternery operator - (condition) ? true stmt : false stmt
+let age = 12
+let res = (age>=18) ? console.log(`You are eligible to vote`) : console.log('Your are not eligible');
