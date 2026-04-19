97b1568f59e35d3e9d7f0aab476e8bd74a1c54d1
Tanvee Dighe <tanvidighe1356@gmail.com>
2026-04-19 22:35:55 +0530

    Add Function_CODE.js with function examples
    Added a comprehensive JavaScript file demonstrating various types of functions, including named functions, anonymous functions, IIFs, nested functions, arrow functions, higher-order functions, and callback functions.

---
+++ Funtion_CODE.js
@@ -0,0 +1,87 @@
+// ! Function : block of code used to perform particular task and it will be executed when we call it 
+
+// console.log("Hello Functions");
+// let a = 10
+
+// // parameters : variables declared at the time of function declaration
+// function demo(n1,n2){
+//     // console.log(n1+n2);
+//     return n1+n2;
+// }
+
+// demo(2,4)
+// console.log(demo(10,30));
+// console.log(demo(90,70));
+// arguments : values passed to the function parameters at the time of function calling
+
+
+// ! Types of functions
+
+// ! Named function/function declaration : The function with name
+// function task(){
+//     console.log("Hello I am function");
+// }
+
+// task()
+
+// ! Anonymous function : The function without name
+// ! Function with expression/functional expression : Passing whole anonymous function as a value to a variable is known as function with expression.
+// first-class function/first-citizen function : function as avlue to a variable
+// let greet = function (user){
+//     console.log(`Hello ${user}`);
+// }
+
+// greet("Tanvi")
+
+// ! Immediate invoking function (IIF) : The function which shd be called immediately after the declaration
+// (function(){
+//     console.log("Hello I am IIF");
+// })()
+
+// ! Nested function : The function inside one more function
+// function parent(){
+//     let pland = 30
+//     let phouse = 10
+//     console.log(`Parent properties : ${pland} acers land and ${phouse} houses`);
+//     function child(){
+//         let cland = 10
+//         let chouse = 2
+//         console.log(`Child properties : ${cland+pland} acers land and ${chouse} houses`);
+//         // lexical scoping / scope chaining : ability of js to access a variable in outer scope whenever it is not there in the current scope
+//         // closure : stores only required data
+//     }
+//     // child() 
+//     return child
+// }
+// parent()()     //javascript currying : Calling nested function along with parent by using one more paranthesis
+
+
+// ! Arrow function : reduces syntax
+// syntax : (parameters)=>{code}
+
+// let add = (x,y) =>  x+y 
+// console.log(add(2,4));
+
+// let task = () => {
+//     let uname = 'ram'
+//     let address = "bng"
+//     return `Hello I am ${uname} from ${address}`
+// }
+
+// console.log(task());
+
+// ! Higher order function(HOF) : The function which takes one more function as a value to its parameter or returns new function
+// using HOF we can perform multiple tasks
+
+// ! Callback function : The function which is passed as an argument to one more function
+
+function operation(a,b,task){
+    let res = task(a,b)
+    return res;
+}
+
+let add = operation(2,3,function(x,y){return x+y})
+console.log(add);
+
+let mul = operation(4,6,(p,q)=> p*q )
+console.log(mul); 
