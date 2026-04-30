482ba71e4d4cffa07b7ed3d4cf942d39fc71aef9
Tanvee Dighe <tanvidighe1356@gmail.com>
2026-04-30 22:19:38 +0530

    Add string reversal logic in string_reverse.js

---
+++ string_reverse.js
@@ -0,0 +1,6 @@
+let str = "Hello good evening";
+
+// Logic: String -> Array -> Reversed Array -> Joined String
+let reversedStr = str.split('').reverse().join('');
+
+console.log(reversedStr); // Output: "gnineve doog olleH"
