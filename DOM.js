4d867c665f67e0675953c4ad2eaa804ed14b8b58
Tanvee Dighe <tanvidighe1356@gmail.com>
2026-04-30 22:25:28 +0530

    Add initial HTML structure to DOM.js

---
+++ DOM.js
@@ -0,0 +1,33 @@
+//document object model
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BOM</title>
+</head>
+<body>
+    
+    <img src="../ASSETS/beautiful-flowers-pictures-free-download.webp" width="200px" alt="">
+    <img src="../ASSETS/beautiful-flowers-pictures-free-download.webp" width="200px" alt="">
+
+    <a href="#">click</a>
+    <a href="#">click</a>
+
+    <form action="">
+        <input type="text">
+        <button>Submit</button>
+    </form>
+
+
+    <h1 id="demo">Hello Good Evening...</h1>
+
+    <div class="box">Hello</div>
+    <div class="box">Hello</div>
+    <div class="box">Hello</div>
+    <div class="box">Hello</div>
+
+    <script src="./bom.js"></script>
+</body>
+</html> 
