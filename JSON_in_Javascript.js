d8b7aa15ed2f4c2b6f9bfb7e4962beeb3c7988ae
Tanvee Dighe <tanvidighe1356@gmail.com>
2026-05-06 21:19:08 +0530

    Comment out promise and storage examples in JS

--- JSON_in_Javascript.js
+++ JSON_in_Javascript.js
@@ -67,24 +67,93 @@
 // taken mock -- fulfilled
 // didnot attend mock -- rejected
 
-let myPromise = new Promise((res,rej)=>{
-    let mock = true;
+// let myPromise = new Promise((res,rej)=>{
+//     let mock = true;
 
-    if(mock == true){
-        res("Thank you for attending the mock")
-    }
-    else{
-        rej("Do attend the mock without fail")
-    }
-})
+//     if(mock == true){
+//         res("Thank you for attending the mock")
+//     }
+//     else{
+//         rej("Do attend the mock without fail")
+//     }
+// })
 
-console.log(myPromise);
+// console.log(myPromise);
 
-// ! then() : will be executed whenever promise is in resolved state
-myPromise.then((msg)=>{console.log(msg);})
+// // ! then() : will be executed whenever promise is in resolved state
+// myPromise.then((msg)=>{console.log(msg);})
 
-// ! catch() : will be executed whenever promise is in rejected state
-myPromise.catch((err)=>{console.log(err);})
+// // ! catch() : will be executed whenever promise is in rejected state
+// myPromise.catch((err)=>{console.log(err);})
 
-// ! finally() : either resolved or rejected
-myPromise.finally(()=>{console.log("All the best...");})
+// // ! finally() : either resolved or rejected
+// myPromise.finally(()=>{console.log("All the best...");})
+
+
+// ! async : makes a function to return a promise
+// async function demo(){
+//     return "hello";
+// }
+
+// console.log(demo());
+
+// ! await : makes the function to wait for promise settlement
+// async function getData() {
+//     let res = await fetch("https://fakestoreapi.com/products");
+//     console.log(res);
+//     let data = await res.json()
+//     console.log(data);
+//     console.log(data[0]);
+//     console.log(data[0].title);
+//     for(let obj of data){
+//         // console.log(obj);
+//         console.log(obj.title);
+
+//         let title =  document.createElement("h2")
+//         title.innerHTML = obj.title
+
+//         let p =  document.createElement("p")
+//         p.innerHTML = obj.description
+
+//         let image = document.createElement('img')
+//         image.src = obj.image
+
+//         document.body.append(title,image,p)
+//     }
+// }
+
+// getData()
+
+// ! webstorage
+
+// ! localStorage : store the data permenently
+
+// ? setItem(key,val) : add data
+// localStorage.setItem("developer1",'Rohit')
+// localStorage.setItem("developer2","Manu")
+
+// ? geteItem(key) : access the data
+ document.writeln(localStorage.getItem("developer1"))
+ document.writeln(localStorage.getItem("developer2"))
+
+// ? removeItem(key) : delete a data
+ localStorage.removeItem('developer1')
+
+// ? clear() : clears the storage -- deletes all the data
+ localStorage.clear()
+
+// ! sessionStorage : stores the data not permenently
+
+// ? setItem(key,val) : add data
+ sessionStorage.setItem("developer1",'Rohit')
+ sessionStorage.setItem("developer2","Manu")
+
+// ? geteItem(key) : access the data
+document.writeln(sessionStorage.getItem("developer1"))
+document.writeln(sessionStorage.getItem("developer2"))
+
+// ? removeItem(key) : delete a data
+sessionStorage.removeItem('developer1')
+
+// ? clear() : clears the storage -- deletes all the data
+sessionStorage.clear() 
